07523de77f360a4a08686f77f61969abcb45deff
Nikita <jetscreamm@gmail.com>
2021-04-15 15:40:37 +0300

    Сreated types of specializations

---
+++ Assets/__Scripts/Spetialization.cs
@@ -0,0 +1,24 @@
+﻿namespace __Scripts
+{
+    public enum Spetialization 
+    {
+        Warrior,
+        Rogue,
+        Wizard,
+        BattleMage,
+        Priest,
+        Paladin,
+        Bard,
+        Fighter,
+        Ranger,
+        SpecialistWizard,
+        Illusionist,
+        Cleric,
+        Druid,
+        Thief,
+        Barbarian,
+        Sorcerer,
+        Monk,
+        Tank
+    }
+}
