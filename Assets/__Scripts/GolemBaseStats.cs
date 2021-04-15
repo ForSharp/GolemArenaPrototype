f1c7b45ba37bd625aa165bc4b8926816bade3989
Nikita <jetscreamm@gmail.com>
2021-04-15 13:36:48 +0300

    Commit

---
+++ Assets/__Scripts/GolemBaseStats.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace __Scripts
+{
+    public struct GolemBaseStats 
+    {
+        public float Strength { get; set; }
+        
+        public float Agility { get; set; }
+        
+        public float Intelligence { get; set; }
+    }
+}
