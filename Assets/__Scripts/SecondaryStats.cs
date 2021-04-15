d17dad52c55f7c84d2a014f24e8f1ab9ee1499fd
Nikita <jetscreamm@gmail.com>
2021-04-15 13:46:47 +0300

    Commit

--- Assets/__Scripts/SecondaryStats.cs
+++ Assets/__Scripts/SecondaryStats.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SecondaryStats : MonoBehaviour
+﻿namespace __Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum SecondaryStats 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Health,
+        Stamina,
+        DamagePerHeat,
+        MoveSpeed,
+        AttackSpeed,
+        Defence,
+        MagicDamage,
+        ManaPool, 
+        AvoidChance,
+        DodgeChance,
+        MagicResistance
     }
 }
