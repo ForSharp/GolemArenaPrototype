52a0e8888e7784036a09a8562c1ecae72c1fedd0
Nikita <jetscreamm@gmail.com>
2021-04-16 09:49:54 +0300

    Started creating a system to change extra stats

---
+++ Assets/__Scripts/ExtraStatsCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace __Scripts
+{
+    public class ExtraStatsCalculator : MonoBehaviour
+    {
+        private float _strength;
+        private float _agility;
+        private float _intelligence;
+
+        public ExtraStatsCalculator(GolemBaseStats stats)
+        {
+            _strength = stats.Strength;
+            _agility = stats.Agility;
+            _intelligence = stats.Intelligence;
+        }
+
+        public float GetHealth()
+        {
+            return (_strength * 0.8f + _agility * 0.1f + _intelligence * 0.1f) * 10;
+        }
+
+        // public float GetStamina()
+        // {
+        //     
+        // }
+    }
+}
---
+++ Assets/__Scripts/GolemExtraStats.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace __Scripts
+{
+    public struct GolemExtraStats 
+    {
+        public float Health { get; set; }
+        
+        public float Stamina { get; set; }
+        
+        public float DamagePerHeat { get; set; }
+        
+        public float MoveSpeed { get; set; }
+        
+        public float AttackSpeed { get; set; }
+        
+        public float Defence { get; set; }
+        
+        public float MagicDamage { get; set; }
+        
+        public float ManaPool { get; set; }
+        
+        public float AvoidChance { get; set; }
+        
+        public float DodgeChance { get; set; }
+        
+        public float MagicResistance { get; set; }
+        
+        public float HitAccuracy { get; set; }
+        
+        public float MagicAccuracy { get; set; }
+        
+        public float RegenerationRate { get; set; }
+    }
+}
