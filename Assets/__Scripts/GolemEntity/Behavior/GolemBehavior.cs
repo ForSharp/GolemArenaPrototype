c4f2345597f6f2bd535ff1522316858c045c8651
Nikita <jetscreamm@gmail.com>
2021-05-02 16:45:52 +0300

    Created a system of behavior changing

--- Assets/__Scripts/GolemEntity/Behavior/GolemBehavior.cs
+++ Assets/__Scripts/GolemEntity/Behavior/GolemBehavior.cs
@@ -1,11 +1,32 @@
-﻿namespace __Scripts.GolemEntity.Behavior
+﻿using UnityEngine;
+
+namespace __Scripts.GolemEntity.Behavior
 {
-    public abstract class GolemBehavior
+    public abstract class GolemBehavior : MonoBehaviour
     {
         protected IMovable _movable;
         protected IHitable _hitable;
         protected IDefencable _defencable;
         protected ICastable _castable;
+
+        public void ChangeMoving(IMovable movable)
+        {
+            _movable = movable;
+        }
         
+        public void ChangeHitting(IHitable hitable)
+        {
+            _hitable = hitable;
+        }
+        public void ChangeDefencing(IDefencable defencable)
+        {
+            _defencable = defencable;
+        }
+        public void ChangeCasting(ICastable castable)
+        {
+            _castable = castable;
+        }
+
+        protected abstract void InitBehaviors();
     }
 }
---
+++ Assets/__Scripts/GolemEntity/Behavior/NoCastBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using __Scripts.ExtraStats;
+using __Scripts.GolemEntity.Behavior;
+using UnityEngine;
+
+public class NoCastBehavior : ICastable
+{
+    public void Cast(GolemExtraStats extraStats)
+    {
+        //Do nothing
+    }
+}
