f3e7b1a5e65b26404a8571d2bfd06b811e5da1b3
Nikita <jetscreamm@gmail.com>
2021-05-10 08:09:23 +0300

    Added animation during navigation

--- Assets/Scripts/TestScripts/NavMeshTester.cs
+++ Assets/Scripts/TestScripts/NavMeshTester.cs
@@ -7,14 +7,36 @@ public class NavMeshTester : MonoBehaviour
     public Transform endPoint;
 
     private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
 
     private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        //_animator.SetFloat("Forward", 1f, 0.1f, Time.deltaTime);
+        _animator.SetFloat("Forward", 0.5f);
     }
 
     private void Update()
     {
         _navMeshAgent.SetDestination(endPoint.position);
+        
+        //_animator.SetFloat("Forward", 1f);
+        
+        
+        if (_navMeshAgent.isStopped)
+        {
+            _animator.SetFloat("Forward", 0f);
+        }
+        
+        // if (!_navMeshAgent.isStopped)
+        // {
+        //     _animator.SetFloat("Forward", 0.5f);
+        // }
+        
+        if (!_navMeshAgent.isStopped)
+        {
+            _animator.SetFloat("Forward", 1f);
+        }
     }
 }
