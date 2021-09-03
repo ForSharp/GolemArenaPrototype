685cd856fee5338245e755552abbe52b78ed401d
Nikita <jetscreamm@gmail.com>
2021-09-03 13:48:45 +0300

    Added sounds and fight logger

--- Assets/Scripts/Fight/AttackHitEventArgs.cs
+++ Assets/Scripts/Fight/AttackHitEventArgs.cs
@@ -9,13 +9,15 @@ namespace Fight
         public readonly float HitAccuracy;
         public readonly RoundStatistics Statistics;
         public readonly float AttackerRotationY;
+        public readonly string AttackerName;
 
-        public AttackHitEventArgs(float damagePerHit, float hitAccuracy, RoundStatistics statistics, float attackerRotationY)
+        public AttackHitEventArgs(float damagePerHit, float hitAccuracy, RoundStatistics statistics, float attackerRotationY, string attackerName)
         {
             DamagePerHit = damagePerHit;
             HitAccuracy = hitAccuracy;
             Statistics = statistics;
             AttackerRotationY = attackerRotationY;
+            AttackerName = attackerName;
         }
     }
 }
---
+++ Assets/Scripts/GameLoop/FightEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Fight;
+
+namespace GameLoop
+{
+    public class FightEventArgs : EventArgs
+    {
+        public readonly AttackHitEventArgs AttackHitEventArgs;
+        public readonly string Target;
+        public readonly bool IsAttackFromBehind;
+        public readonly bool IsAvoiding;
+
+        public FightEventArgs(AttackHitEventArgs attackHitEventArgs, string target, bool isAttackFromBehind, bool isAvoiding = false)
+        {
+            AttackHitEventArgs = attackHitEventArgs;
+            Target = target;
+            IsAttackFromBehind = isAttackFromBehind;
+            IsAvoiding = isAvoiding;
+        }
+    }
+}
